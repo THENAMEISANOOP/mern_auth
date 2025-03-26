b899c160009ea071f9f398e362dbe8e418ef1b5c
THENAMEISANOOP <sanoopanoop444@gmail.com>
2025-03-27 01:10:00 +0530

    create and run server

---
+++ api/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+
+
+app.listen(300,()=>{
+    console.log("server running port 3000 ")
+})
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
+
+
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import Signin from "./pages/Signin"
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold">Hello, Vite + React + Tailwind!</h1>
-    </div>
+    <BrowserRouter>
+    
+    {/* header */}
+    <Header/>
+    
+    <Routes>  
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/sign-in" element={<Signin/>}/>
+      <Route path="/sign-up" element={<SignUp/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
